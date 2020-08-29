907fc23c302343d1274fda242b806d210eab0d9f
sahil2608 <sahilchhikara2608@gmail.com>
2020-08-30 03:34:29 +0530

    heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -141,3 +141,8 @@ SESSION_COOKIE_SECURE = True
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
